2fddbf1f4722b520f7b11baf993dea50a31bb4ca
anand <anand.panchdhari@gmail.com>
2025-04-09 16:06:58 +0530

    Final Commit :)

--- direction_service/src/main/java/com/example/controller/DirectionController.java
+++ direction_service/src/main/java/com/example/controller/DirectionController.java
@@ -4,15 +4,13 @@ import com.example.entity.Direction;
 import com.example.service.DirectionService;
 import com.example.dto.ServiceData;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:8000")
 public class DirectionController {
     @Autowired
     DirectionService service;
